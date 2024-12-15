573937b2198f642632875a343c9c4c14e60b25f0
Rares Sandu <rares.sandu52@gmail.com>
2024-12-15 16:01:46 +0200

    seminar11 + homePractice for exam

---
+++ HomePractice/TestPractice12/app/src/main/java/com/raressandu/testpractice12/DisplayActivity.java
@@ -0,0 +1,14 @@
+package com.raressandu.testpractice12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
